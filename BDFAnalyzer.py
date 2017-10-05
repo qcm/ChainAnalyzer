8df1dc89b6d664e029f73270d9b9e5aec9158e15
Bo-Yi Lee <boyil@qca.qualcomm.com>
2017-10-05 10:58:42 -0700

    First Draft

---
+++ BDFAnalyzer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+#===============================================================================
+#
+# Board Data File Analyzer
+#
+# Copyright (c) 2017 by QUALCOMM Atheros, Incorporated.
+# All Rights Reserved
+# QUALCOMM Atheros Confidential and Proprietary
+#
+# Notifications and licenses are retained for attribution purposes only
+#===============================================================================
+
+
+Description = """
+[Description]:
+Read WLAN board data file and generate graph per chain.
+
+[Usage]:
+BDFAnalyzer.py input.txt
+"""
+
+#CLPC=open("calibration.txt","w")
+#bdf=open("bdwlan30.txt",'r')
+#for line in bdf:
+#        if "fullpdadc" in line:
+#                temp=(line[23:-1])
+#        if "fullmeas_pwr" in line:
+#                CLPC.write (line[:-1])
+#                CLPC.write(',')
+#                CLPC.write(temp)
+#                CLPC.write('\n')
+#
+#bdf.close()
+#CLPC.close()
